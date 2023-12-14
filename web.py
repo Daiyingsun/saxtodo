62f4e285a1b06aa1b2b978390c3fa4652f18a4b4
nahid <ffn400878@gmail.com>
2023-12-15 00:07:10 +0600

    1.Adding todo in the checkboxes.

--- web.py
+++ web.py
@@ -1,4 +1,24 @@
-import streamlit as st 
+import streamlit as st
+import func
+import time
 
-st.title()
+todos = func.get_todos()
 
+
+def add_todo():
+    todo_l = st.session_state['new_todo'] + '\n'
+    todos.append(todo_l)
+    func.write_todos(todos)
+
+
+now = time.strftime("Date-%d %B %Y")
+
+
+st.title("Sax ToDo App")
+st.button(now)
+
+for todo in todos :
+    st.checkbox(todo)
+
+st.text_input(label="",placeholder="Enter new To-Do...",on_change=add_todo,
+              key='new_todo')
