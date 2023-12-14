d6157e73fdeb4e5fe335ab76d55e4a13bdbb72db
Nahid <142040729+Daiyingsun@users.noreply.github.com>
2023-12-15 01:00:31 +0600

    Add files via upload

---
+++ web.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import func
+import time
+
+todos = func.get_todos()
+
+
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    todos.append(todo)
+    func.write_todos(todos)
+
+
+now = time.strftime("Date-%d %B %Y")
+
+
+st.title("Sax ToDo App")
+st.button(now)
+
+for index ,todo in enumerate(todos) :
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox :
+        todos.pop(index)
+        func.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+
+st.text_input(label="",placeholder="Enter new To-Do...",on_change=add_todo,
+              key='new_todo')
